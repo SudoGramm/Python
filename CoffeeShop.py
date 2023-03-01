4eb16b1aff2e1d175864c54b4c9590a59244aaef
SudoGramm <126685306+SudoGramm@users.noreply.github.com>
2023-03-01 15:56:22 +0200

    Update CoffeeShop.py

--- CoffeeShop.py
+++ CoffeeShop.py
@@ -1,6 +1,35 @@
-print("Helo and welcome to the park\n")
+print("Hello and welcome to CC Jitters!")
 
-name = input("What is your name?\n")
+name = input("What is your name?: ")
 
-print("\nHello " + name)
+print("Hello " + name + ", what would you like from the menu\n")
+
+order = input("Latte 2.00$\n" + "Hot Chocolate 2.00$\n" + "Espresso 2.00$ : \n")
+
+espresso = "2.00"
+
+latte = "2.00"
+
+hot_chocolate = "2.00"
+
+print("Ok! Your " + order + " will be ready in a few minutes!")
+
+extra = input("While you're waiting would you like anything else by\nany chance?: \n")
+
+if extra == "no":
+  print("Okay your " + order + " is done!\n")
+else:
+  print("Ok, your " + extra + " will be ready in a few minutes too!\n" + "Your orders are ready!\n")
+
+if extra == "no":
+  print("Your total is 2.00$.")
+else:
+  print("Your total is 4.00$")
+
+  
+
+
+
+  
+
 
